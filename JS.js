488dcf9ac970978fbb5e9a66daee358e4f22d6f3
Richard190104 <119693579+Richard190104@users.noreply.github.com>
2024-06-18 23:19:15 +0200

    Update JS.js

--- JS.js
+++ JS.js
@@ -4,6 +4,8 @@ function toggleMenu() {
     button.classList.toggle('open');
     var slideBar = document.getElementById('slide-bar');
     slideBar.classList.toggle('active');
+    var homepage = document.getElementById('home-page-text');
+    homepage.classList.toggle('moveUp');
     if (slideBar.classList.contains('active')) {
         slideBar.style.animation = 'slideIn 0.5s forwards';
     } else {
