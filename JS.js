4232a37583bfec50b00e1c5b86079a626ae43f28
Richard190104 <119693579+Richard190104@users.noreply.github.com>
2024-06-19 20:31:17 +0200

    Update JS.js

--- JS.js
+++ JS.js
@@ -7,12 +7,40 @@ function toggleMenu() {
     var homepage = document.getElementById('home-page-text');
     homepage.classList.toggle('moveUp');
     if (slideBar.classList.contains('active')) {
-        slideBar.style.animation = 'slideIn 0.5s forwards';
+        slideBar.style.animation = 'slideIn 1s forwards';
     } else {
-        slideBar.style.animation = 'slideOut 0.5s forwards';
+        slideBar.style.animation = 'slideOut 1s forwards';
     }
 
     
 }
 
+function copyrequests(){
+    const parent = document.getElementById('here');
+    const textarea = document.getElementById('requests');
+    const chosen = parent.querySelectorAll('.active');
+    textarea.value = 'Mám záujem o tieto služby:'
+    chosen.forEach(element => {
+        textarea.value +=  '\n' + element.id;
+    });
+    
+}
 
+
+function toggleChoice(button){
+    button.classList.toggle('active');
+    const contact = document.getElementById('c-button');
+
+    const elements = document.querySelectorAll('.active');
+    if(elements.length === 0){
+        contact.classList.remove('show');
+        const textarea = document.getElementById('requests');
+        textarea.value = '';
+
+    }
+    else{
+        
+        contact.classList.add('show');
+    }
+    
+}
